265ca421a3603595b82185383e0e8d604814cae9
Frank Pigeon <frank.pigeonjr@gmail.com>
2014-06-26 11:34:36 -0500

    initiailized dev environment

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -23,12 +23,17 @@ $app = new Illuminate\Foundation\Application;
 | given environment, then we will automatically detect it for you.
 |
 */
+// OLD
+// $env = $app->detectEnvironment(array(
 
-$env = $app->detectEnvironment(array(
+// 	'local' => array('homestead'),
 
-	'local' => array('homestead'),
+// ));
 
-));
+$env = $app->detectEnvironment(function()
+{
+ return !empty($_SERVER['LARAVEL_ENV']) && $_SERVER['LARAVEL_ENV'] == 'local' ? 'local' : 'production';
+});
 
 /*
 |--------------------------------------------------------------------------
