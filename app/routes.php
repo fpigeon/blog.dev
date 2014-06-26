f4b388a2fddb87a842580bb09cf84fbc6099edfb
Frank Pigeon <frank.pigeonjr@gmail.com>
2014-06-26 11:24:02 -0500

    added routes for resume and portfolio

--- app/routes.php
+++ app/routes.php
@@ -10,8 +10,28 @@
 | and give it the Closure to execute when that URI is requested.
 |
 */
+// Route::get('/', function(){
+// 	return 'we are home';
+// });
 
-Route::get('/', function()
-{
-	return View::make('hello');
+
+// Route::get('/sayHello/{name}', function($name)
+// {
+// 	if ($name == "frank")
+//     {
+//         return Redirect::to('/');
+//     }
+//     else
+//     {
+//         return "Hello, $name!";
+//     }
+// });
+
+// Create a route for your resume page at the path '/resume' that returns 'This is my resume'.
+// Create a route for your portfolio page at the path '/portfolio' that returns 'This is my portfolio'.
+Route::get('/resume', function(){
+	return 'This is my resume';
+});
+Route::get('/portfolio', function(){
+	return 'This is my portfolio';
 });
