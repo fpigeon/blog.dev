a660341c871a310f534abaa88dc18d343f9a6fcc
Frank Pigeon <frank.pigeonjr@gmail.com>
2014-07-01 15:34:54 -0500

    added human readble dates

--- app/views/posts/show.blade.php
+++ app/views/posts/show.blade.php
@@ -7,7 +7,9 @@
 <div class="container">
 	<h1>{{{ $post->title }}}</h1>
 	<!-- display post content   -->		
-	<p>	{{{ $post->created_at }}}</p>
+	<h5>	{{{ $post->created_at->setTimezone('America/Chicago')->format('l, F jS Y @ h:i:s A') }}}</h5>
+	<p>	{{{ $post->created_at->diffForHumans() }}}</p>
+
 	<p>{{{ $post->body }}}</p>
 	<a href="{{ action('PostsController@edit', $post->id) }}" class="btn btn-default btn-small">Edit</a>
 </div>
