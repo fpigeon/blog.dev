2d379555804395ce9c49cc2326969b7a6066b581
Frank Pigeon <frank.pigeonjr@gmail.com>
2014-07-07 02:10:14 -0500

    New feature: added a social media landing page

--- app/views/connect.blade.php
+++ app/views/connect.blade.php
@@ -1,29 +1,31 @@
 @extends('layouts.master')
 
 @section('topscript')
-<!-- Custom styles for this template -->
-    <link href="/assets/css/main.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="/assets/css/connect.css" rel="stylesheet">
 @stop
 
 @section('content')
     <div class="container">
         <div class="logo">
-            <img src="/assets/img/logo.png" width="100" alt="BlackTie.co">
+            <img src="/assets/img/avatar.jpg" width="100" class="img-circle" alt="profile">
         </div>
         <div class="row centered">
             <div class="col-lg-8 col-lg-offset-2 w">
-                <h1>BlackTie.co<br/>SMPL Theme.</h1>
+                <h1>Frank<br/>Pigeon</h1>
             </div>
         </div>
         <div class="row centered">
             <div class="col-lg-8 col-lg-offset-2 w tooltip-demo">
-                <a data-toggle="tooltip" title="Dribbble" href="http://dribbble.com/CarlosAlvarez"><i class="fa fa-dribbble"></i></a>
-                <a data-toggle="tooltip" title="Twitter" href="http://twitter.com/BlackTie_co"><i class="fa fa-twitter"></i></a>
-                <a data-toggle="tooltip" title="Premium Themes" href="https://wrapbootstrap.com/user/Basicoh"><i class="fa fa-briefcase"></i></a>
-                <a data-toggle="tooltip" title="Free Themes" href="http://BlackTie.co"><i class="fa fa-heart"></i></a>
-                <a data-toggle="tooltip" title="Blog" href="http://freelance.postach.io"><i class="fa fa-comment"></i></a>
-                <a data-toggle="tooltip" title="Email Me!" href="mailto:carlos@alvarez.is"><i class="fa fa-envelope"></i></a>
-                <a data-toggle="tooltip" title="Newsletter" href="http://eepurl.com/Eck_T"><i class="fa fa-coffee"></i></a>
+                <a data-toggle="tooltip" title="Twitter" href="https://twitter.com/fpigeonjr" target="_blank"><i class="fa fa-twitter"></i></a>
+                <a data-toggle="tooltip" title="Google+" href="https://www.google.com/+FrankPigeon" target="_blank"><i class="fa fa-google-plus"></i></a>
+                <a data-toggle="tooltip" title="Facebook" href="http://www.facebook.com/frank.pigeon" target="_blank"><i class="fa fa-facebook"></i></a>
+                <a data-toggle="tooltip" title="Linkedin" href="https://www.linkedin.com/in/frankpigeonjr" target="_blank"><i class="fa fa-linkedin"></i></a>
+                <a data-toggle="tooltip" title="GitHub" href="https://github.com/fpigeon" target="_blank"><i class="fa fa-github"></i></a>
+                <a data-toggle="tooltip" title="Blog" href="http://blog.dev"><i class="fa fa-comment" target="_blank"></i></a>
+                <a data-toggle="tooltip" title="Instagram" href="http://instagram.com/frankpigeonjr" target="_blank"><i class="fa fa-instagram"></i></a>
+                <a data-toggle="tooltip" title="Youtube" href="http://www.youtube.com/user/frankpigeonjr" target="_blank"><i class="fa fa-youtube"></i></a>
+                <a data-toggle="tooltip" title="Email" href="mailto:frank.pigeonjr@gmail.com" target="_blank"><i class="fa fa-envelope"></i></a>
             </div>
         </div>
     </div><!-- /.container -->
@@ -32,7 +34,7 @@
         <div class="row w centered">
             <br>
             <div class="col-lg-6 col-lg-offset-3">
-                <p>Believe in yourself! Have faith in your abilities! Without a humble but reasonable confidence in your own powers you cannot be successful or happy.</p>
+                <p>Web Developer, Tech Enthusiast, US Army veteran, bass player, father, husband, servant</p>
             </div>
         </div>
     </div>
