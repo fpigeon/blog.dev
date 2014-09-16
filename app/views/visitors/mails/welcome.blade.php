6d373971ab76575572530ca208293704b96eb69e
Frank Pigeon <frank.pigeonjr@gmail.com>
2014-09-16 12:58:16 -0500

    Customized subject to update email.

--- app/views/visitors/mails/welcome.blade.php
+++ app/views/visitors/mails/welcome.blade.php
@@ -2,6 +2,5 @@
 
 <p>{{ $first_name }} has registered on Happy Realtor.</p>
 <p>
-	Find out more on the visitors page.
-	<a href="http://frankpigeon.com/visitors">Visitors Page</a>
+	Find out more on the <a href="http://frankpigeon.com/visitors">Visitors Page</a>.
 </p>
