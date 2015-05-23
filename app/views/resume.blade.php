c5e1c7fdafa7d98eb48dd8af79071031dc3d834f
Frank Pigeon <frank@amdesign.com>
2015-05-22 23:21:44 -0500

    updates resume, adds rollover styles,  and activates links.

--- app/views/resume.blade.php
+++ app/views/resume.blade.php
@@ -57,7 +57,7 @@
                 <div class="col-lg-12">
                     <!-- banner has text -->
                     <h1>Frank S. Pigeon Jr.</h1>
-                    <h3>Full Stack Web Developer | frank.pigeonjr@gmail.com</h3>
+                    <h3>Full Stack Web Developer</h3>
                 </div><!--/.col-lg-12 -->
             </div><!--/.row -->
         </div><!--/.container -->
@@ -93,12 +93,24 @@
                 <div class="col-lg-2 col-lg-offset-1">
                     <h5>WORK</h5>
                 </div>
-
                 <div class="col-lg-6">
-                    <p><t>Full Stack Web Developer</t><br/>
-                        Self-Employed <br/>
+                    <p><t>Web Developer</t><br/>
+                        AM Design <br/>
                     </p>
-                    <p><more>Platform/Environment: Hypertext Markup Language (HTML), Linux, Mac OSX, VirtualBox, Vagrant, Ansible, Apache, mySQL, PHP, Git, GitHub, JavaScript, jQuery, Cascading Style Sheets (CSS), Bootstrap framework, WordPress Content Management System (CMS), Moment.js API, Adobe Photoshop, Adobe Illustrator, and Adobe Premiere Pro.</more></p>
+                    <p><more>Platform/Environment: Hypertext Markup Language (HTML5), Linux, Mac OSX, VirtualBox, Vagrant, Apache, MySQL, PHP, Git, Stash, Grunt, Gulp, JavaScript, jQuery, Cascading Style Sheets (CSS), SASS, WordPress Content Management System (CMS), Harvest, and the Adobe Creative Suite.</more></p>
+                    <p>
+                        Responsibilities involve focusing on design, quality, and functionality. Deliver websites that accurately match the design files provided by design team. Ensure all work is thoroughly tested before presented to the customer. Adhere to the quality standards and principles developed by AM Design. Take the time to improve skills and continue to learn. Present alternatives for any limitations of the medium. Work with the team on web projects to develop cross-platform websites and applications, including mobile solutions. Maintain the support of our active sites in bug fixes and any other issues that arise. Assist in social media/online strategy sessions Track development time in relation to project scope. Reports to the lead developer. Proficient in PHP, HTML, CSS, JavaScript, JQuery. Familiar with the Adobe Suite (Photoshop, Illustrator, Etc). Must be able to develop sites from technical specifications & graphical representations.
+                    </p>
+                </div>
+                <div class="col-lg-3">
+                    <p><sm>NOV 2014 - CURRENT</sm></p>
+                </div>
+
+                <div class="col-lg-6 col-lg-offset-3">
+                    <p><t>Full Stack Web Developer</t><br/>
+                        Self-Employed
+                    </p>
+                    <p><more>Platform/Environment: Hypertext Markup Language (HTML), Linux, Mac OSX, VirtualBox, Vagrant, Ansible, Apache, mySQL, PHP, Git, GitHub, JavaScript, jQuery, Cascading Style Sheets (CSS), Bootstrap framework, WordPress Content Management System (CMS), Adobe Photoshop, Adobe Illustrator, and Adobe Premiere Pro.</more></p>
                     <p><a href="http://community-helpers.com" target="_blank">Community Helpers</a>: application that connects seniors (givers) that need work done around their home and the youth (helpers) that want to earn some extra money. Built using Laravel, PHP, MySQL, JavaScript, jQuery, and Twitter Bootstrap. Development done in a Vagrant environment. Version control using Git and GitHub. Features a helper/giver dashboard, a job listing for helpers to apply to, and giving the power to the giver to select a helper based on their jobs completed and reviews.</p>
                     <p><a href="http://frankpigeon.com/posts" target="_blank">Laravel Blog</a>: application for writing and viewing blog posts as well as my portfolio, resume, and social media landing page. Built using Laravel, PHP, MySQL, JavaScript, jQuery, Twitter Bootstrap, CKeditor text editor plugin. Development done in a Vagrant environment. Version control using Git and GitHub. Features user login, blog post creation and viewing, image upload, search functionality, and ability for authors to edit posts.</p>
                     <p><a href="{{{ action('HomeController@showWhack') }}}" target="_blank" >Whack-A-Mole</a>: first-person video game based on the classic whack-a-mole game. The application features the ability to keep score, maintain a high-score, and a countdown timer. This application was built using PHP, JavaScript, and jQuery.</p>
@@ -110,9 +122,9 @@
 
                 <div class="col-lg-6 col-lg-offset-3">
                     <p><t>IT Project Manager</t><br/>
-                        Operations Training Support, G6, AMEDDC&S <br/>
+                        Operations Training Support, G6, AMEDDC&S
                     </p>
-                    <p><more>Provides project management support during the planning, development, testing and implementation of new systems in support of the Army Medical Department, Military Health Systems, and the Medical Command. Support includes project planning, control, reporting, risk management, scheduling, tracking, variance analysis and administrative support throughout the life cycle of the project.</more></p>
+                    <p>Provides project management support during the planning, development, testing and implementation of new systems in support of the Army Medical Department, Military Health Systems, and the Medical Command. Support includes project planning, control, reporting, risk management, scheduling, tracking, variance analysis and administrative support throughout the life cycle of the project.</p>
                 </div>
                 <div class="col-lg-3">
                     <p><sm>JUNE 2010 - CURRENT</sm></p>
@@ -120,43 +132,43 @@
 
                 <div class="col-lg-6 col-lg-offset-3">
                     <p><t>IT Specialist</t><br/>
-                        Operations Support, G6, AMEDDC&S <br/>
+                        Operations Support, G6, AMEDDC&S
                     </p>
-                    <p><more>Coordinates deployment services provided by the local Network Enterprise Center (NEC) to AMEDDC&S functional users. Manages AMEDDC&S involvement in the initial deployment, subsequent movement, and technology refresh of computer equipment, printers, copiers, and telephones</more></p>
+                    <p>Coordinates deployment services provided by the local Network Enterprise Center (NEC) to AMEDDC&S functional users. Manages AMEDDC&S involvement in the initial deployment, subsequent movement, and technology refresh of computer equipment, printers, copiers, and telephones</p>
                 </div>
                 <div class="col-lg-3">
                     <p><sm>MARCH 2008 - JUNE 2010</sm></p>
                 </div>
                 <div class="col-lg-6 col-lg-offset-3">
                     <p><t>Forward Deployed Tech</t><br/>
-                        Defense Medical Readiness Training Institute <br/>
+                        Defense Medical Readiness Training Institute
                     </p>
-                    <p><more>Responsible for the daily operations of the Information Management Office as resident technical expert on all ADPE (Automated Data Processing Equipment), communications products, and audio / visual equipment, and networks assigned to the DMRTI command spanning four buildings and two installations (Ft. Sam Houston and Camp Bullis) in a senior joint military environment.</more></p>
+                    <p>Responsible for the daily operations of the Information Management Office as resident technical expert on all ADPE (Automated Data Processing Equipment), communications products, and audio / visual equipment, and networks assigned to the DMRTI command spanning four buildings and two installations (Ft. Sam Houston and Camp Bullis) in a senior joint military environment.</p>
                 </div>
                 <div class="col-lg-3">
                     <p><sm>JUNE 2006 - FEBRUARY 2008</sm></p>
                 </div>
                 <div class="col-lg-6 col-lg-offset-3">
                     <p><t>Systems Engineer</t><br/>
-                        Directorate of Information Management <br/>
+                        Directorate of Information Management
                     </p>
-                    <p><more>Responsible for the deployment, installation, configuration, and operation of all IT equipment acquired through the DOIM working independently or on a team of IT specialists in a dynamic, fast paced environment while maintaining DOD (Department of Defense) standards and guidelines. Installed, configured, repaired, deployed, troubleshoot personal computers and peripheral devices, and operating systems.</more></p>
+                    <p>Responsible for the deployment, installation, configuration, and operation of all IT equipment acquired through the DOIM working independently or on a team of IT specialists in a dynamic, fast paced environment while maintaining DOD (Department of Defense) standards and guidelines. Installed, configured, repaired, deployed, troubleshoot personal computers and peripheral devices, and operating systems.</p>
                 </div>
                 <div class="col-lg-3">
                     <p><sm>MARCH 2005 - JUN 2006</sm></p>
                 </div>
                 <div class="col-lg-6 col-lg-offset-3">
                     <p><t>Systems Research Specialist</t><br/>
-                        U.S. Army South <br/>
+                        U.S. Army South
                     </p>
-                    <p><more>Responsible for the deployment, installation, configuration, and operation of all IT equipment acquired through the DOIM working independently or on a team of IT specialists in a dynamic, fast paced environment while maintaining DOD (Department of Defense) standards and guidelines. Installed, configured, repaired, deployed, troubleshoot personal computers and peripheral devices, and operating systems.</more></p>
+                    <p>Responsible for the deployment, installation, configuration, and operation of all IT equipment acquired through the DOIM working independently or on a team of IT specialists in a dynamic, fast paced environment while maintaining DOD (Department of Defense) standards and guidelines. Installed, configured, repaired, deployed, troubleshoot personal computers and peripheral devices, and operating systems.</p>
                 </div>
                 <div class="col-lg-3">
                     <p><sm>JULY 2004 - MARCH 2005</sm></p>
                 </div>
                 <div class="col-lg-6 col-lg-offset-3">
                     <p><t>Staff Sargeant</t><br/>
-                        U.S. Army <br/>
+                        U.S. Army
                     </p>
                     <p><more>Served in various positions during military career:</more></p>
                     <ul>
@@ -199,7 +211,7 @@
                     </p>
                 </div>
                 <div class="col-lg-3">
-                    <p><sm>2012-2013</sm><br/>
+                    <p><sm>2012-2013</sm>
                     </p>
                 </div>
 
@@ -215,7 +227,7 @@
 
                 <div class="col-lg-6 col-lg-offset-3">
                     <p><t>A.A.S., Computer Information Systems</t><br/>
-                        Kansas City Kansas Community College <br/>
+                        Kansas City Kansas Community College
                     </p>
                 </div>
                 <div class="col-lg-3">
@@ -236,7 +248,7 @@
                 </div>
                 <div class="col-lg-6">
                     <p><t>Project + Certified Professional</t><br/>
-                        CompTIA <br/>
+                        CompTIA
                     </p>
                 </div>
                 <div class="col-lg-3">
@@ -283,14 +295,14 @@
                 </div>
                 <div class="col-lg-6">
                     <p><t>Email</t><br/>
-                        frank.pigeonjr@gmail.com <br/>
+                        <a class="footer-link" href="mailto:frank.pigeonjr@gmail.com ">frank.pigeonjr@gmail.com</a>
                     </p>
-                    <p><t>Address</t><br/>
-                        3630 Stanley Road <br/>
-                        Ft. Sam Houston, TX 78234<br/>
-                    </p>
+                    <address><t>Address</t><br/>
+                        8251 Bedford Euless Rd #250<br/>
+                        North Richland Hills, TX 76180<br/>
+                    </address>
                     <p><t>Phone</t><br/>
-                        210-704-7443 <br/>
+                         <a class="footer-link" href="tel:210-704-7443">210-704-7443</a>
                     </p>
                 </div>
                 <div class="col-lg-3">
