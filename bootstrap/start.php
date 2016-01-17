30c0f3ee5bde96efa285e931c19738c255cb7676
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2016-01-17 00:50:06 -0600

    env

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -23,10 +23,9 @@ $app = new Illuminate\Foundation\Application;
 | given environment, then we will automatically detect it for you.
 |
 */
-$env = $app->detectEnvironment(function()
-{
-    return $_SERVER['APP_ENV'];
-});
+$env = $app->detectEnvironment(array(
+    'local' => array('homestead'),
+));
 /*
 |--------------------------------------------------------------------------
 | Bind Paths
